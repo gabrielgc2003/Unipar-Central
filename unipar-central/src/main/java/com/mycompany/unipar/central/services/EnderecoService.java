1b053aec4084f64ef0fac8955acd2a209f9ae75f
Zambello Rodrigo <101841098+ZambelloRodrigo@users.noreply.github.com>
2023-06-16 13:16:05 -0300

    Enviando novamente o enderecoService

--- unipar-central/src/main/java/com/mycompany/unipar/central/services/EnderecoService.java
+++ unipar-central/src/main/java/com/mycompany/unipar/central/services/EnderecoService.java
@@ -4,6 +4,8 @@ import com.mycompany.unipar.central.exceptions.CampoLimiteTamanhoException;
 import com.mycompany.unipar.central.exceptions.CampoNaoInformadoException;
 import com.mycompany.unipar.central.exceptions.EntidadeNaoInformadaException;
 import com.mycompany.unipar.central.models.Endereco;
+import com.mycompany.unipar.central.repositories.EnderecoDAO;
+import java.sql.SQLException;
 
 public class EnderecoService {
     public void validar(Endereco endereco) throws EntidadeNaoInformadaException, CampoLimiteTamanhoException, CampoNaoInformadoException {
@@ -38,8 +40,33 @@ public class EnderecoService {
         if (endereco.getCep().length() > 8){
             throw new CampoLimiteTamanhoException("Endereco(Cep)", "8");
         }
-
-
-
     }
+    
+    public Endereco findById(int id) throws SQLException, CampoLimiteTamanhoException, Exception{
+        if(id <= 0)
+            throw  new CampoLimiteTamanhoException("id","1");
+        EnderecoDAO enderecoDAO = new EnderecoDAO();
+        Endereco retorno = enderecoDAO.findById(id);
+
+        if (retorno == null)
+            throw new Exception("Não foi possível encontrar um endereco" +
+                    "com id " + id + " tente outro código.");
+        return retorno;
+   }
+
+   public void insert(Endereco endereco) throws SQLException, CampoLimiteTamanhoException, EntidadeNaoInformadaException, Exception{
+        validar(endereco);
+        EnderecoDAO enderecoDAO = new EnderecoDAO();
+        enderecoDAO.insert(endereco);
+   }
+   public void update(Endereco endereco) throws SQLException, CampoLimiteTamanhoException, EntidadeNaoInformadaException, Exception{
+        validar(endereco);
+        EnderecoDAO enderecoDAO = new EnderecoDAO();
+        enderecoDAO.update(endereco);
+   }
+
+   public void delete(int id) throws SQLException, Exception{
+        EnderecoDAO enderecoDAO = new EnderecoDAO();
+        enderecoDAO.delete(id);
+   }
 }
